77fbe859f237817de446daa5c52237f72fe3d42a
zhjc1124 <zhang1124@gmail.com>
2020-09-24 11:08:33 +0800

    fix

--- ser.py
+++ ser.py
@@ -50,8 +50,8 @@ def pi_work(fsk_device, back_device):
             time.sleep(2)
             continue
         if platform.system == 'Linux':
-            # os.system('ntpdate -u 0.debian.pool.ntp.org')
-            os.system('date -s "$(wget -qSO- --max-redirect=0 www.baidu.com 2>&1 | grep Date: | cut -d' ' -f5-8)Z"')
+            sos.system('ntpdate -u 0.debian.pool.ntp.org')
+            # os.system('date -s "$(wget -qSO- --max-redirect=0 www.baidu.com 2>&1 | grep Date: | cut -d' ' -f5-8)Z"')
             # os.system('systemctl stop serial-getty@ttyACM0.service')
             # os.system('systemctl stop serial-getty@ttyUSB0.service')
         break
