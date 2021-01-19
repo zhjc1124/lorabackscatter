13db01b91ee87743e9dc0c3f07cd5f65f5c457f0
zhjc1124 <zhjc1124@gmail.com>
2021-01-19 11:51:22 +0800

    update ser.py

--- ser.py
+++ ser.py
@@ -25,25 +25,30 @@ logger.addHandler(fhlr)
 real_path = os.path.dirname(os.path.realpath(__file__)) + os.sep
 
 def check_device():    
-    rx_device = []
+    active_device = None
+    passive_device = None
     nbiot_device = None
     ports = list_ports.comports()
     for i in ports:
-        if i.location in ['1-1.1']:
+        if not i.location:
+            continue
+        if i.location.startswith('1-1.1'):
             nbiot_device = i.device
-        elif i.location and i.location.startswith('1-1.'):
-            rx_device.append(i.device)
+        elif i.location.startswith('1-1.2'):
+            active_device = i.device
+        elif i.location.startswith('1-1.4'):
+            passive_device = i.device
     if not nbiot_device:
         logging.error('Nb-IoT not detected')
         os._exit(0)
     else:
         logging.info('Nb-IoT detected: {}'.format(nbiot_device))
-    if not rx_device:
+    if not active_device and not passive_device:
         logging.error('Reciever not detected')
         os._exit(0)
-    elif len(rx_device):
-        logging.info('{} reciever detected {}'.format(len(rx_device), rx_device))
-    return rx_device, nbiot_device
+    logging.info('{} active reciever detected'.format(active_device))
+    logging.info('{} passive reciever detected'.format(passive_device))
+    return active_device, passive_device, nbiot_device
 
 def display(w_string):
     try:
@@ -59,40 +64,52 @@ def display(w_string):
     logging.info('Shock status: {}'.format(sensors[3]))
     
 
-def pi_work(rx_device, nbiot_device):
+def pi_work(active_device, passive_device, nbiot_device):
+
     while True:
         try:
             requests.get('http://www.baidu.com')
+            flag = 1
         except:
             time.sleep(2)
-            continue
-        if platform.system == 'Linux':
+            flag = 2
+        if platform.system == 'Linux' and flag == 1:
             os.system('ntpdate -u 0.debian.pool.ntp.org')
             # os.system('date -s "$(wget -qSO- --max-redirect=0 www.baidu.com 2>&1 | grep Date: | cut -d' ' -f5-8)Z"')
             # os.system('systemctl stop serial-getty@ttyACM0.service')
             # os.system('systemctl stop serial-getty@ttyUSB0.service')
             logging.info('Time synced')
+        elif flag == 2:
+            l = nbiot_ser.readline()
+            print(l)
+            os._exit(0)
         break
-    rx_sers = []
-    for i in rx_device:
-        rx_sers.append(serial.Serial(i, 9600))
     nbiot_ser = serial.Serial(nbiot_device, 9600)
+    active_device = serial.Serial(active_device, 9600)
+    passive_device = serial.Serial(passive_device, 9600)
     logging.info('Open seial')
     filename = None
     current_time = time.time()
-    rx_nums = [0]*len(rx_sers)
+    rx_sers = [active_device, passive_device]
+    rx_counts = {}
     while True:
         for index, rx_ser in enumerate(rx_sers):
+            
             wait = rx_ser.in_waiting
             # print(wait)
-            if rx_ser.in_waiting < 6:
+            if wait < 6:
                 continue
             # print(wait)
             # print(rx_ser)
             line = rx_ser.readline()
             # print(line)
             logging.info('{}'.format(line))
-            rx_nums[index] += 1
+            
+            if line in rx_counts:
+                rx_counts[line] += 1
+            else:
+                rx_counts[line] = 1
+
             stamp = time.time()
             if not (line.startswith(b'txtest')) and not (line.endswith(b'\r\n') and len(line) == 8):
                 continue
@@ -118,9 +135,12 @@ def pi_work(rx_device, nbiot_device):
                     filename = t.decode() + '.log'
                 with open(real_path + filename, 'ab+') as f:
                     f.write(f_string)
-            if rx_nums[index] == [10, 1][len(rx_sers)==1]:
-                rx_nums[index] = 0
+            if index == 1:
                 nbiot_ser.write(w_string)
+            elif rx_counts[line] > 10:
+                nbiot_ser.write(w_string)
+                rx_counts[line] = 0
+                
             display(w_string)
 
 
